3d22b1ce0764170ee8de78a4216c12409662a6d6
Yu Asabe <yuasabe@nextriya.com>
2014-11-12 01:19:05 +0900

    removed unnecessary files

--- clubs/circles/junior/tekken/js/main.js
+++
@@ -1,5 +0,0 @@
-$("#menu-icon a").click(function(){
-  $("#mainnavi").slideToggle("fast","linear");
-  $("#menu-icon a").toggleClass("menu-on");
-  return false;
-});
--- events/enju/2013/maps/QR向け/readme.txt
+++
@@ -1,4 +0,0 @@
-配置場所：
-http://shibumaku.jp/mk-student/enjufes2013/bunnjitsu2013/sp/index.html
-
-URL編集忘れずに！
